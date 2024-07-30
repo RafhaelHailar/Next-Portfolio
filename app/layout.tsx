a18b3db77fc25a917db459e8412c2a576190fcb4
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-30 20:32:07 +0800

    [Update] default theme dark.

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className + " flex overflow-hidden"}>
+      <body className={inter.className + " flex overflow-hidden dark"}>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
