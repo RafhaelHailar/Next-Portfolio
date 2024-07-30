c2957c3e1d0c6e1635950c98dedc2d0f324926f4
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-30 18:59:48 +0800

    [Feat] added query provider.

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+import QueryProvider from "@/providers/QueryProvider";
+
+const inter = Montserrat({ weight: ["700"], subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rafhael Hailar",
+  description: "Rafhael Hailar Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className + " flex overflow-hidden"}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
+    </html>
+  );
+}
--- services/queries.ts
+++ services/queries.ts
@@ -1,1 +1,11 @@
-import api from "./axios";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getProjects } from "./api";
+
+export const useGetProjects = () => {
+    return useQuery({
+        queryKey: ["projects"],
+        queryFn: getProjects
+    })
+};
